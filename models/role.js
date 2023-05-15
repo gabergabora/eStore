bb94f42dc83621005e3274d84331391f566aad0d
gabergabora <102705568+gabergabora@users.noreply.github.com>
2023-05-15 21:56:59 +0300

    Update role.js

--- models/role.js
+++ models/role.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-
+                  
 const roleSchema = new Schema({
     name: { type: String, required: true },
     permissions: [{ type: Schema.Types.ObjectId, 'ref': 'permission' }],
